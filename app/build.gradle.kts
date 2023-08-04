4be9acc0f4f10a8506ccb611bfa0585b67ca4a2f
TheKeeperOfPie <thekeeperofpie@gmail.com>
2023-08-04 02:55:39 -0700

    Bump to versionCode 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         applicationId = "com.thekeeperofpie.anichive"
         minSdk = 28
         targetSdk = 33
-        versionCode = 1
+        versionCode = 2
         versionName = "0.39"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
