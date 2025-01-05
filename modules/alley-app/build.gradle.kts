6418af4b8ead7e8d971506de2cd3f8bed01987f9
TheKeeperOfPie <thekeeperofpie@gmail.com>
2025-01-05 17:00:31 -0500

    Disable new WASM exception handling for better compatibility

--- modules/alley-app/build.gradle.kts
+++ modules/alley-app/build.gradle.kts
@@ -152,7 +152,7 @@ kotlin {
             }
         }
         freeCompilerArgs.add("-Xcontext-receivers")
-        freeCompilerArgs.add("-Xwasm-use-new-exception-proposal")
+//        freeCompilerArgs.add("-Xwasm-use-new-exception-proposal")
     }
 
     sourceSets {
