fe20f7de5167a9ab70447ae196c5254ccfada038
TheKeeperOfPie <thekeeperofpie@gmail.com>
2025-03-03 19:10:06 -0500

    Handle escape key for alley wasmJs

--- modules/alley-app/src/wasmJsMain/kotlin/com/thekeeperofpie/artistalleydatabase/alley/app/main.kt
+++ modules/alley-app/src/wasmJsMain/kotlin/com/thekeeperofpie/artistalleydatabase/alley/app/main.kt
@@ -1,11 +1,14 @@
 package com.thekeeperofpie.artistalleydatabase.alley.app
 
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.backhandler.BackGestureDispatcher
+import androidx.compose.ui.backhandler.LocalBackGestureDispatcher
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.window.ComposeViewport
@@ -33,9 +36,14 @@ import com.thekeeperofpie.artistalleydatabase.utils_compose.navigation.LocalNavi
 import com.thekeeperofpie.artistalleydatabase.utils_compose.navigation.rememberNavigationController
 import kotlinx.browser.document
 import kotlinx.browser.window
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.isActive
 import okio.Buffer
 import okio.fakefilesystem.FakeFileSystem
 import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.w3c.dom.events.Event
+import org.w3c.dom.events.KeyboardEvent
 
 private val fakeFileSystem = FakeFileSystem()
 
@@ -44,6 +52,20 @@ private lateinit var artistImageCache: ArtistImageCache
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalBrowserHistoryApi::class)
 fun main() {
     ComposeViewport(document.body!!) {
+        val keyEvents = remember {
+            Channel<KeyboardEvent>(capacity = 5, onBufferOverflow = BufferOverflow.DROP_OLDEST)
+        }
+        DisposableEffect(keyEvents) {
+            val callback: (Event) -> Unit = {
+                if (it is KeyboardEvent) {
+                    keyEvents.trySend(it)
+                }
+            }
+
+            document.addEventListener("keydown", callback)
+            onDispose { document.removeEventListener("keydown", callback) }
+        }
+
         val scope = rememberCoroutineScope()
         val component = ArtistAlleyWasmJsComponent::class.create(scope)
         artistImageCache = component.artistImageCache
@@ -100,15 +122,40 @@ fun main() {
 
             val navHostController = rememberNavController()
             val navigationController = rememberNavigationController(navHostController)
+            val backGestureDispatcher = remember { WasmJsBackGestureDispatcher() }
+            LaunchedEffect(backGestureDispatcher, keyEvents) {
+                while (isActive) {
+                    val event = keyEvents.receive()
+                    backGestureDispatcher.onKeyboardEvent(event)
+                }
+            }
+
             CompositionLocalProvider(
                 LocalWindowConfiguration provides windowConfiguration,
                 LocalNavigationController provides navigationController,
+                LocalBackGestureDispatcher provides backGestureDispatcher,
             ) {
                 ArtistAlleyAppScreen(component, navHostController)
                 LaunchedEffect(navHostController) {
                     window.bindToNavigation(navHostController)
                 }
             }
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+private class WasmJsBackGestureDispatcher: BackGestureDispatcher() {
+    fun onKeyboardEvent(event: KeyboardEvent) {
+        if (event.key == "Escape") {
+            onBack()
+        }
+    }
+
+    private fun onBack() {
+        activeListener?.let {
+            it.onStarted()
+            it.onCompleted()
         }
     }
 }
