8f70a12486164ce77b0ab8477161036b0376f770
TheKeeperOfPie <thekeeperofpie@gmail.com>
2025-05-04 11:04:28 -0400

    Try to fix linkage error

--- modules/alley/src/wasmJsMain/kotlin/com/thekeeperofpie/artistalleydatabase/alley/settings/ImportExportUtils.wasmJs.kt
+++ modules/alley/src/wasmJsMain/kotlin/com/thekeeperofpie/artistalleydatabase/alley/settings/ImportExportUtils.wasmJs.kt
@@ -1,12 +1,16 @@
 package com.thekeeperofpie.artistalleydatabase.alley.settings
 
 import com.thekeeperofpie.artistalleydatabase.utils.DateTimeUtils
-import io.github.vinceglb.filekit.FileKit
-import io.github.vinceglb.filekit.download
+import io.ktor.util.toJsArray
+import kotlinx.browser.document
 import kotlinx.browser.window
 import kotlinx.datetime.Clock
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
+import kotlinx.dom.createElement
+import org.w3c.dom.HTMLAnchorElement
+import org.w3c.dom.url.URL
+import org.w3c.files.File
 
 actual object ImportExportUtils {
     actual suspend fun download(text: String) {
@@ -14,8 +18,19 @@ actual object ImportExportUtils {
             Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date
         )
         val fileName = "ArtistAlley-$dateSuffix.json"
-        FileKit.download(text.encodeToByteArray(), fileName)
+        val array = JsArray<JsAny?>()
+        array[0] = text.encodeToByteArray().toJsArray()
+
+        val file = File(fileBits = array, fileName = fileName)
+        val element = document.createElement("a") {
+            this as HTMLAnchorElement
+            href = URL.createObjectURL(file)
+            download = fileName
+        } as HTMLAnchorElement
+
+        element.click()
     }
 
-    actual fun getImportUrl(exportPartial: String) = "${window.location.origin}#import=$exportPartial"
+    actual fun getImportUrl(exportPartial: String) =
+        "${window.location.origin}#import=$exportPartial"
 }
