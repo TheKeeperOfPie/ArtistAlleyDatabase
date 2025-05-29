81e4753e808c525159b046f5090cc0e378ea918b
TheKeeperOfPie <thekeeperofpie@gmail.com>
2025-05-28 22:21:44 -0400

    Disable new exception handling again

--- modules/alley-app/build.gradle.kts
+++ modules/alley-app/build.gradle.kts
@@ -165,7 +165,7 @@ kotlin {
             }
         }
         freeCompilerArgs.add("-Xcontext-receivers")
-        freeCompilerArgs.add("-Xwasm-use-new-exception-proposal")
+//        freeCompilerArgs.add("-Xwasm-use-new-exception-proposal")
     }
 
     sourceSets {
