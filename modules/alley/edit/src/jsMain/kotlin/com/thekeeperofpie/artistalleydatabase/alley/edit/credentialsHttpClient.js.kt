4645fcef366ba6ef42c5df5ed78bebc71796748b
TheKeeperOfPie <thekeeperofpie@gmail.com>
2026-03-20 03:58:32 -0400

    Only include credentials for prodImagesUrl requests

--- modules/alley/edit/src/jsMain/kotlin/com/thekeeperofpie/artistalleydatabase/alley/edit/credentialsHttpClient.js.kt
+++ modules/alley/edit/src/jsMain/kotlin/com/thekeeperofpie/artistalleydatabase/alley/edit/credentialsHttpClient.js.kt
@@ -1,13 +1,23 @@
 package com.thekeeperofpie.artistalleydatabase.alley.edit
 
+import com.eygraber.uri.Uri
+import com.thekeeperofpie.artistalleydatabase.alley.utils.AlleyUtils
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.js.Js
+import io.ktor.client.fetchOptions
+import io.ktor.client.plugins.HttpSend
+import io.ktor.client.plugins.plugin
+import org.w3c.fetch.INCLUDE
+import org.w3c.fetch.RequestCredentials
 
-actual fun credentialsHttpClient() = HttpClient(Js) {
-    HttpClient()
-    engine {
-        configureRequest {
-            credentials = "include"
+private val imagesHost = Uri.parseOrNull(AlleyUtils.prodImagesUrl)?.host
+
+actual fun credentialsHttpClient() = HttpClient().apply {
+    plugin(HttpSend).intercept {
+        if (it.url.host == imagesHost) {
+            it.fetchOptions {
+                credentials = RequestCredentials.INCLUDE
+            }
         }
+        execute(it)
     }
 }
--- modules/alley/edit/src/wasmJsMain/kotlin/com/thekeeperofpie/artistalleydatabase/alley/edit/credentialsHttpClient.wasmJs.kt
+++ modules/alley/edit/src/wasmJsMain/kotlin/com/thekeeperofpie/artistalleydatabase/alley/edit/credentialsHttpClient.wasmJs.kt
@@ -1,13 +1,23 @@
 package com.thekeeperofpie.artistalleydatabase.alley.edit
 
+import com.eygraber.uri.Uri
+import com.thekeeperofpie.artistalleydatabase.alley.utils.AlleyUtils
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.js.Js
+import io.ktor.client.fetchOptions
+import io.ktor.client.plugins.HttpSend
+import io.ktor.client.plugins.plugin
+import org.w3c.fetch.INCLUDE
+import org.w3c.fetch.RequestCredentials
 
-@OptIn(ExperimentalWasmJsInterop::class)
-actual fun credentialsHttpClient() = HttpClient(Js) {
-    engine {
-        configureRequest {
-            credentials = "include".toJsString()
+private val imagesHost = Uri.parseOrNull(AlleyUtils.prodImagesUrl)?.host
+
+actual fun credentialsHttpClient() = HttpClient().apply {
+    plugin(HttpSend).intercept {
+        if (it.url.host == imagesHost) {
+            it.fetchOptions {
+                credentials = RequestCredentials.INCLUDE
+            }
         }
+        execute(it)
     }
 }
