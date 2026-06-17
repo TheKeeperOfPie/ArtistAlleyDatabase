9b9a8f17e35e5be0acb44fbe40351b007e4ce053
TheKeeperOfPie <thekeeperofpie@gmail.com>
2026-06-16 23:09:20 -0400

    Highlight favorites in artists changelog

--- modules/alley/src/commonMain/kotlin/com/thekeeperofpie/artistalleydatabase/alley/changelog/ArtistChangelogViewModel.kt
+++ modules/alley/src/commonMain/kotlin/com/thekeeperofpie/artistalleydatabase/alley/changelog/ArtistChangelogViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.thekeeperofpie.artistalleydatabase.alley.artist.ArtistEntryDao
+import com.thekeeperofpie.artistalleydatabase.alley.database.UserEntryDao
 import com.thekeeperofpie.artistalleydatabase.alley.series.SeriesEntryCache
 import com.thekeeperofpie.artistalleydatabase.alley.settings.ArtistAlleySettings
 import com.thekeeperofpie.artistalleydatabase.shared.alley.data.DataYear
@@ -12,6 +13,7 @@ import com.thekeeperofpie.artistalleydatabase.utils_compose.getOrPut
 import dev.zacsweers.metro.Assisted
 import dev.zacsweers.metro.AssistedInject
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.stateIn
@@ -24,6 +26,7 @@ class ArtistChangelogViewModel(
     dispatchers: CustomDispatchers,
     val seriesEntryCache: SeriesEntryCache,
     val settings: ArtistAlleySettings,
+    userEntryDao: UserEntryDao,
     @Assisted savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
 
@@ -32,10 +35,20 @@ class ArtistChangelogViewModel(
     private val randomSeed =
         savedStateHandle.getOrPut("randomSeed") { Random.nextInt().absoluteValue }
 
-    internal val changes = catalogsOnly.mapLatest { artistEntryDao.getChangelog(it) }
-        .mapLatest {
-            // TODO: Split by year
-            it.map { it.toChangelogEntry(DataYear.ANIME_EXPO_2026, randomSeed, false) }
+    internal val changes = combine(userEntryDao.getTagFavorites(), catalogsOnly, ::Pair)
+        .mapLatest { (tagFavorites, catalogsOnly) ->
+            artistEntryDao.getChangelog(catalogsOnly)
+                .asSequence()
+                .map {
+                    it.toChangelogEntry(
+                        // TODO: Split by year
+                        dataYear = DataYear.ANIME_EXPO_2026,
+                        randomSeed = randomSeed,
+                        showOnlyConfirmedTags = catalogsOnly,
+                        seriesIdsToHighlight = tagFavorites.seriesIds,
+                        merchIdsToHighlight = tagFavorites.merchIds,
+                    )
+                }
                 .groupBy { it.date }
                 .toList()
                 .sortedByDescending { it.first }
@@ -47,6 +60,7 @@ class ArtistChangelogViewModel(
                         updated = updated.sortArtistsForChangelog(),
                     )
                 }
+                .toList()
         }
         .flowOn(dispatchers.io)
         .stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
