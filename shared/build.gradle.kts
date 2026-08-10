a388abf732cc5f4675a10f0985b13f55b9770235
TheKeeperOfPie <thekeeperofpie@gmail.com>
2026-08-09 22:51:03 -0400

    Update dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 plugins {
     id("org.jetbrains.kotlin.multiplatform") version "2.4.10"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.4.10"
-    id("com.android.kotlin.multiplatform.library") version "9.4.0-alpha07"
+    id("com.android.kotlin.multiplatform.library") version "9.4.0-alpha08"
 }
 
 group = "com.thekeeperofpie.artistalleydatabase.shared"
